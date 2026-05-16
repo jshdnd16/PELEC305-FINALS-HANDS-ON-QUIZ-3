ae6066e6a2298e5f749524cc2ec1de8cbe758614
Joshua <joshuadenado0316@gmail.com>
2026-05-16 19:40:16 +0800

    first commit

---
+++ backend/tasks/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'is_completed']   # columns shown in the list
+    list_filter  = ['is_completed']                   # filter sidebar
+    search_fields = ['title']                         # search bar
+    list_editable = ['is_completed']                  # toggle directly from the list
